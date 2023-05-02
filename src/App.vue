54f7da46e45e3c8a6ff859ec99d696dfe806a78f
ABiviano13 <abiviano0@outlook.com>
2023-05-02 16:19:28 +0200

    Start

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Projects from './components/Projects.vue';
+
+export default{
+  components: {
+    Projects
+  }
+}
+</script>
+
+<template>
+
+  <div class="container">
+    <Projects />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
